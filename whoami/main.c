9f867138bd2999bd122bb47de67a853f56e8d931
Stepan Repin <stnrepin@gmail.com>
2022-11-29 16:17:33 +0300

    Update whoami to use events

--- whoami/main.c
+++ whoami/main.c
@@ -13,38 +13,39 @@ enum states : int {
     STATE_END = 4,
 };
 
-int main(void) {
-    uint8_t data;
-    enum states cur_state = STATE_A;
-    const char * const diags[] = {
-        [STATE_A] = "- Who am I?\n",
-        [STATE_B] = "- You are Rei Ayanami.\n",
-        [STATE_C] = "- But, who are you? Are you Rei Ayanami as well?\n",
-        [STATE_D] = ("- Correct, I’m the thing that is recognized as Rei Ayanami. "
-                     "We are all things that are recognized as Rei Ayanami.\n")
-    };
+enum states cur_state;
+const char * const diags[] = {
+    [STATE_A] = "- Who am I?\n",
+    [STATE_B] = "- You are Rei Ayanami.\n",
+    [STATE_C] = "- But, who are you? Are you Rei Ayanami as well?\n",
+    [STATE_D] = ("- Correct, I’m the thing that is recognized as Rei Ayanami. "
+                    "We are all things that are recognized as Rei Ayanami.\n")
+};
 
-    sys_init();
+static void handler(sys_ev_id_t ev) {
+    switch (ev) {
+        case SYS_EV_INIT:
+            cur_state = STATE_A;
+            sys_uart_puts("\n");
+            break;
 
-    sys_uart_puts("\n");
-
-    while(1) {
-        data = sys_lradc_data();
-        if (data == 0x09) {
+        case SYS_EV_BUT0_UP:
             sys_uart_puts(diags[cur_state]);
             cur_state++;
-            if (cur_state == STATE_END) {
-                break;
-            }
-        }
-
-        sys_delay(100 * 1000); // 100ms
+            break;
     }
 
-    sys_uart_puts("\n\n");
-    sys_uart_puts(art_read());
+    if (cur_state == STATE_END) {
+        sys_uart_puts("\n\n");
+        sys_uart_puts(art_read());
+        return;
+    }
 
-    while(1);
+    sys_add_handler(handler, SYS_EV_BUT0_UP);
+}
 
-    return 0;
+int main(void) {
+    sys_init();
+    sys_init_handler(handler);
+    return sys_run();
 }
